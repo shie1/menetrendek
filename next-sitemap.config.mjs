5e412381eac5ee1f66a72dbf2236cbf241e0bfe4
Shie1 <sonkolyw@gmail.com>
2022-11-03 18:12:08 +0100

    typo... ffs

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-    siteUrl: process.env.SITE_URL || 'https://menetrendek.ifno',
+    siteUrl: process.env.SITE_URL || 'https://menetrendek.info',
     generateRobotsTxt: true,
 }
 
