e74361e1481bd3f30e8f7b5f7fb3706f02043a08
Shie1bi <sonkolyw@gmail.com>
2022-12-09 11:18:38 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { Title, Container, Stack, Group, Text, Divider } from "@mantine/core";
-import { IconLayout, IconSearch, IconShare, IconApps } from "@tabler/icons";
+import { Title, Container, Stack, Text } from "@mantine/core";
+import { IconLayout, IconSearch, IconShare, IconApps, IconRotateClockwise } from "@tabler/icons";
 import type { NextPage } from "next";
 import { FeaturesGrid } from "../components/hello";
 
@@ -9,14 +9,15 @@ const Home: NextPage = () => {
             <Title>Menetrendek</Title>
             <Title mt={-8} style={{ fontSize: '1.4rem' }} color="dimmed" order={2}>A modern menetrend kereső</Title>
             <Title mt={-2} color="dimmed" style={{ fontSize: '1.1rem' }} order={3} >MÁV, Volánbusz, BKK, GYSEV, MAHART, BAHART</Title>
-            <Text  style={{fontSize: '1rem'}} color="dimmed" weight={600}>Íme néhány dolog, amiben egyszerűen jobbak vagyunk:</Text>
+            <Text style={{ fontSize: '1rem' }} color="dimmed" weight={600}>Íme néhány dolog, amiben egyszerűen jobbak vagyunk:</Text>
         </Stack>
         <FeaturesGrid
             data={[
                 { title: "Kezelőfelület", icon: IconLayout, description: "Modern, letisztult és mobilbarát kezelőfelület." },
                 { title: "Gyors elérés", icon: IconSearch, description: "Egyszerű megálló- és állomáskeresés, a legutóbbi elemek mentése gyors elérésbe." },
                 { title: "Megosztás", icon: IconShare, description: "Útvonaltervek gyors megosztása kép formájában." },
-                { title: "PWA támogatás", icon: IconApps, description: "Ez a weboldal egy PWA (progresszív webalkalmazás), így könnyen letöltheted alkalmazásként a telefonodra." }
+                { title: "PWA támogatás", icon: IconApps, description: "Ez a weboldal egy PWA (progresszív webalkalmazás), így könnyen letöltheted alkalmazásként a telefonodra." },
+                { title: "Aktív fejlesztés", icon: IconRotateClockwise, description: "A weboldal szinte minden héten frissül. A funkciók folyamatosan bővülnek, a hibák folyamatosan keresve és javítva vannak." }
             ]}
         />
     </Container>)
