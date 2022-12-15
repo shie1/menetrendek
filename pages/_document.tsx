8e00e4b10db808da3c9d7b232faa587c593dd3f7
Shie1 <sonkolyw@gmail.com>
2022-12-15 19:19:14 +0100

    Google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -54,10 +54,25 @@ const Document: NextPage = () => {
                 strategy="beforeInteractive"
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3057716180157458"
             />
+
+            <Script id="google-tag-manager" strategy="afterInteractive" /*Google tag manager*/>
+                {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-MVHLMXV');
+                `}
+            </Script>
         </Head>
         <body>
             <Main />
             <NextScript />
+            <noscript
+                dangerouslySetInnerHTML={{
+                    __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MVHLMXV" height="0" width="0" style="display: none; visibility: hidden;" />`,
+                }}
+            />
         </body>
     </Html >)
 }
