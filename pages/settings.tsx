2476219f2ce21fab8634434cc06096a445647e19
Shie1 <sonkolyw@gmail.com>
2022-12-24 15:23:48 +0100

    settings: raised segmentedcontrol size on mobile

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -38,7 +38,7 @@ const Settings: NextPage = () => {
                     <Text size="md">Itt kiválaszthatod, hogy melyik naptár szolgáltást használod.</Text>
                     <Text mt={-4} size="sm">Készülékedhez javasolt: {ua?.device.vendor === "Apple" ? "Egyéb (ICS)" : "Bármely"}</Text>
                     <Stack justify="center">
-                        <SegmentedControl fullWidth orientation={segmentedBreak ? "vertical" : "horizontal"} value={cookies["calendar-service"] || '1'} onChange={(e) => setCookie("calendar-service", e, { path: '/', maxAge: 60 * 60 * 24 * 365 })} data={[{ label: "Google Calendar", value: '1' }, { label: "Outlook", value: '2' }, { label: "Office 365", value: '3' }, { label: "Yahoo", value: '4' }, { label: "Egyéb (ICS)", value: '5' }]} />
+                        <SegmentedControl size={segmentedBreak ? "md" : "sm"} fullWidth orientation={segmentedBreak ? "vertical" : "horizontal"} value={cookies["calendar-service"] || '1'} onChange={(e) => setCookie("calendar-service", e, { path: '/', maxAge: 60 * 60 * 24 * 365 })} data={[{ label: "Google Calendar", value: '1' }, { label: "Outlook", value: '2' }, { label: "Office 365", value: '3' }, { label: "Yahoo", value: '4' }, { label: "Egyéb (ICS)", value: '5' }]} />
                     </Stack>
                 </Stack>
             </ContentCard>
