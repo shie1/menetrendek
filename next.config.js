16c890536db6992574a2b62f341074d9d82207a4
Shie1 <sonkolyw@gmail.com>
2022-12-26 20:15:00 +0100

    twa

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const prod = process.env.NODE_ENV === 'production'
 const runtimeCaching = require('next-pwa/cache')
 const withPWA = require('next-pwa')({
   dest: "public",
-  disable: !prod,
+  customWorkerDir: "serviceworker",
+//  disable: !prod,
   register: true,
   skipWaiting: true,
   runtimeCaching,
