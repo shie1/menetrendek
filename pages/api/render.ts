d348a03c3a7598436320a8d2b1532503e37319ea
Shie1 <sonkolyw@gmail.com>
2022-12-28 04:42:56 +0100

    now

--- pages/api/render.ts
+++ pages/api/render.ts
@@ -7,7 +7,7 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
 ) {
-    const browser = await launch({ headless: true })
+    const browser = await launch({ headless: true, args: ["--no-sandbox"] })
     const page = await browser.newPage();
     await page.setViewport({
         width: 3000,
