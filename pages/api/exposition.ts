df46da5f6d69aaa047d134c0255920048a504ec7
Shie1 <sonkolyw@gmail.com>
2023-01-07 23:32:35 +0000

    minimal api translation

--- pages/api/exposition.ts
+++ pages/api/exposition.ts
@@ -7,5 +7,6 @@ export default async function handler(
     res: NextApiResponse<any>
 ) {
     const rb = JSON.parse(req.body)
-    res.status(200).json(await exposition(rb["runs"], rb["nativeData"], rb["datestring"]))
+    const lang = req.headers.host?.split('.')[0] === 'en' ? 'en' : 'hu'
+    res.status(200).json(await exposition(rb["runs"], rb["nativeData"], rb["datestring"], lang))
 }
--- pages/api/routes.ts
+++ pages/api/routes.ts
@@ -7,6 +7,7 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
 ) {
-    const rb = JSON.parse(req.body) 
-    res.status(200).json(await routes(rb))
+    const rb = JSON.parse(req.body)
+    const lang = req.headers.host?.split('.')[0] === 'en' ? 'en' : 'hu'
+    res.status(200).json(await routes(rb, lang))
 }
