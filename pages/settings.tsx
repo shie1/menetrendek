e9df7f11fad9673c2c9972e43314c0ea3fbfd7cc
Shie1 <sonkolyw@gmail.com>
2023-01-08 16:06:31 +0100

    translated ics

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -60,7 +60,7 @@ const Settings: NextPage = (props: any) => {
                     <Text size="md">{strings.calendarService}</Text>
                     <Text mt={-4} size="sm">{strings.recommendedForDevice}: {ua?.device.vendor === "Apple" ? `${strings.other} (ICS)` : strings.any}</Text>
                     <Stack justify="center">
-                        <SegmentedControl size={segmentedBreak ? "md" : "sm"} fullWidth orientation={segmentedBreak ? "vertical" : "horizontal"} value={cookies["calendar-service"] || '1'} onChange={(e) => setCookie("calendar-service", e, { path: '/', maxAge: 60 * 60 * 24 * 365 })} data={[{ label: "Google Calendar", value: '1' }, { label: "Outlook", value: '2' }, { label: "Office 365", value: '3' }, { label: "Yahoo", value: '4' }, { label: "Egyéb (ICS)", value: '5' }]} />
+                        <SegmentedControl size={segmentedBreak ? "md" : "sm"} fullWidth orientation={segmentedBreak ? "vertical" : "horizontal"} value={cookies["calendar-service"] || '1'} onChange={(e) => setCookie("calendar-service", e, { path: '/', maxAge: 60 * 60 * 24 * 365 })} data={[{ label: "Google Calendar", value: '1' }, { label: "Outlook", value: '2' }, { label: "Office 365", value: '3' }, { label: "Yahoo", value: '4' }, { label: `${strings.other} (ICS)`, value: '5' }]} />
                     </Stack>
                 </Stack>
             </ContentCard>
