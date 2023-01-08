3cc381089a681699953712163d5c19e9c9266586
Shie1 <sonkolyw@gmail.com>
2023-01-08 16:09:22 +0100

    PLEASE FOR THE LOVE OF GOD

--- components/api.tsx
+++ components/api.tsx
@@ -1,5 +1,5 @@
 export const getHost = (req: any) => {
-    return req ? (process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://" + req.headers.host) : ""
+    return req ? (process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://" + req.headers.host) : typeof window !== 'undefined' ? window.location.origin : ""
 }
 
 export const apiCall = async (method: "GET" | "POST", url: string, body?: any) => {
