bc7ff3209e2a79de15d483b43a0834a69ea24eae
Shie1 <sonkolyw@gmail.com>
2023-01-09 18:09:29 +0100

    please xd

--- components/menu.tsx
+++ components/menu.tsx
@@ -11,6 +11,7 @@ import { DatePicker } from "@mantine/dates";
 import { showNotification } from "@mantine/notifications";
 import { motion } from "framer-motion"
 import "dayjs/locale/hu"
+import { LocalizedStrings } from "../pages/api/localization";
 
 export const QuickMenu = ({ strings }: { strings: LocalizedStrings }) => {
     const { selection, setSelection, input } = useContext(Input)
@@ -61,8 +62,8 @@ export const QuickMenu = ({ strings }: { strings: LocalizedStrings }) => {
         <motion.div transition={{ duration: .2 }} animate={{ margin: !loading ? 0 : '-10px -10px', padding: !loading ? 0 : '10px 10px' }} style={{ position: 'relative' }}>
             <Group sx={{ display: 'flex', flexWrap: "wrap", '& > *': { flex: 8 }, '& .searchInput': { minWidth: '16rem', } }}>
                 <LoadingOverlay transitionDuration={200} radius="md" visible={loading} />
-                <StopInput variant="from" />
-                <StopInput variant="to" />
+                <StopInput strings={strings} variant="from" />
+                <StopInput strings={strings} variant="to" />
                 <DatePicker locale="hu" transition="scale-y" transitionDuration={200} styles={(theme) => ({ dropdown: { borderRadius: theme.radius.md, border: '1px solid', borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2] }, input: { fontSize: 16, border: 'unset', background: 'transparent' }, wrapper: { width: '100%', height: 36 }, root: { borderBottom: '3px solid #373A40', height: 45, display: 'flex', alignItems: 'center', } })} className="searchInput" value={date || new Date()} clearable={false} onChange={setDate} />
                 <Button variant="gradient" onClick={(e) => {
                     e.preventDefault()
