46fc9267367c38bcd8239c9d7b7c38c14ab9ee5d
Shie1 <sonkolyw@gmail.com>
2023-01-15 13:17:37 +0100

    Changed sitemap

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -3,12 +3,6 @@ const config = {
     siteUrl: process.env.SITE_URL || 'https://menetrendek.info',
     generateRobotsTxt: true,
     exclude: ['/render', '/settings', '/route', '/routes', '/runs'],
-    alternateRefs: [
-        {
-            href: 'https://en.menetrendek.info',
-            hreflang: 'en',
-        },
-    ]
 }
 
 export default config
