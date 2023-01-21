a9b0bda898a5801e72c2a5139470af808ba377bd
Shie1 <sonkolyw@gmail.com>
2023-01-21 23:32:49 +0100

    share and calendar

--- pages/routes.tsx
+++ pages/routes.tsx
@@ -2,23 +2,68 @@ import type { NextPage } from "next"
 import { apiCall } from "../components/api"
 import { dateString, exposition, route } from "../client"
 import { PageHeading } from "../components/page"
-import { IconLine } from "@tabler/icons"
-import { Accordion, Skeleton, Timeline } from "@mantine/core"
+import { IconCalendarEvent, IconLine, IconShare } from "@tabler/icons"
+import { Accordion, ActionIcon, Group, Loader, Skeleton, Timeline } from "@mantine/core"
 import { useMyAccordion } from "../components/styles"
 import { ActionBullet, RouteExposition, RouteSummary } from "../components/routes"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { yahoo, office365, google, ics, outlook } from "calendar-link";
+import { useRouter } from "next/router"
+import { useCookies } from "react-cookie"
 
-const Route = ({ route }: { route: route }) => {
+const cal = (service: number, body: any) => {
+    window.open((() => {
+        switch (service) {
+            case 1:
+                return google(body)
+            case 2:
+                return outlook(body)
+            case 3:
+                return office365(body)
+            case 4:
+                return yahoo(body)
+            case 5:
+            default:
+                return ics(body)
+        }
+    })(), "_blank")
+}
+
+const Route = ({ route, index }: { route: route, index: any }) => {
     const [exposition, setExposition] = useState<Array<exposition>>([])
+    const router = useRouter()
+    const [file, setFile] = useState<File | undefined>()
+    const [body, setBody] = useState<any>()
+    const [cookies] = useCookies(["calendar-service"])
+
+    useEffect(() => {
+        if (!body && exposition.length) {
+            const start = new Date(`${router.query['d'] || dateString(new Date())} ${route.departureTime}`)
+            const end = new Date(`${router.query['d'] || dateString(new Date())} ${route.arrivalTime}`)
+            setBody({
+                start,
+                end,
+                description: `${exposition.join("\n")}`,
+                location: exposition[0].station,
+                title: `${route.departure[0]} - ${route.arrival[0]}`
+            })
+        }
+    }, [body, exposition])
 
     return (<>
         <Accordion.Control onClick={() => {
-            apiCall("POST", "/api/exposition", { exposition: route.expositionData.exposition, nativeData: route.expositionData.nativeData, date: dateString(new Date()) }).then((e) => setExposition(e.exposition))
+            apiCall("POST", "/api/exposition", { exposition: route.expositionData.exposition, nativeData: route.expositionData.nativeData, date: dateString(new Date()) }).then(async (e) => {
+                setExposition(e.exposition)
+                const id = Date.now().toString()
+                const image = `/api/render?${router.asPath.split('?')[1]}&d=${router.query['d'] || dateString(new Date())}&i=${index}`
+                const blob = await (await fetch(image)).blob()
+                setFile(new File([blob], `menetrendek-${id}.jpeg`, { type: "image/jpeg" }))
+            })
         }}>
             <RouteSummary item={route} />
         </Accordion.Control>
         <Accordion.Panel>
-            {!exposition.length ? (<Timeline active={Infinity}>{Array(route.expositionData.exposition.runcount * 2).fill(0).map((v, i, arr) => {
+            {!exposition.length ? (<><Timeline active={Infinity}>{Array(route.expositionData.exposition.runcount * 2).fill(0).map((v, i, arr) => {
                 if (i + 1 === arr.length) {// Last element
                     return (<Timeline.Item bullet={<ActionBullet muvelet="leszállás" network={1} />}>
                         <Skeleton radius="lg" height={53} />
@@ -28,7 +73,29 @@ const Route = ({ route }: { route: route }) => {
                         <Skeleton radius="lg" height={i % 2 === 0 ? 98 : 120} />
                     </Timeline.Item>)
                 }
-            })}</Timeline>) : <RouteExposition exposition={exposition} />}
+            })}</Timeline>
+                <Group position="right">
+                    <Loader size={28} />
+                </Group>
+            </>) : <>
+                <RouteExposition exposition={exposition} />
+                <Group position="right">
+                    <ActionIcon role="button" aria-label="Hozzáadás a naptárhoz" onClick={() => cal(Number(cookies["calendar-service"]), body)}>
+                        <IconCalendarEvent />
+                    </ActionIcon>
+                    {!file ? <Loader size={28} /> :
+                        <ActionIcon role="button" aria-label="Megosztás" onClick={() => {
+                            const params: any = {
+                                from: router.query.from,
+                                to: router.query.to,
+                                index: index,
+                            }
+                            navigator.share({ files: [file], url: `https://menetrendek.info/route?${(new URLSearchParams(params)).toString()}`, "title": body.text })
+                        }}>
+                            <IconShare />
+                        </ActionIcon>}
+                </Group>
+            </>}
         </Accordion.Panel>
     </>)
 }
@@ -40,7 +107,7 @@ const Routes: NextPage = (props: any) => {
         <Accordion classNames={classes}>
             {props.routes.routes.map((route: any, i: any) => (
                 <Accordion.Item my="sm" key={i.toString()} value={i.toString()}>
-                    <Route route={route} />
+                    <Route index={i} route={route} />
                 </Accordion.Item>
             ))}
         </Accordion>
