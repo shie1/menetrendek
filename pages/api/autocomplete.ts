ab9b3be748bce39b6cb233238d4b7ee3d7b478ac
Shie1 <sonkolyw@gmail.com>
2023-01-26 17:53:54 +0100

    removed a log

--- pages/api/autocomplete.ts
+++ pages/api/autocomplete.ts
@@ -10,7 +10,6 @@ export default async function handler(
     let cities: Array<any> = []
     let inp = `^${input.replace(/\s$/g, '').replace(/,\s/g, ' ')}`
     const results = await query('SELECT c.name as city, s.id as id, CONCAT(c.name, ", ", s.name) as stop_name, network FROM menetrendek_info.stops as s LEFT JOIN cities as c ON s.city=c.id WHERE CONCAT(c.name, " ", s.name) REGEXP ? LIMIT 50;', [inp])
-    console.log(results[0])
     await Promise.all(results.map(async (stop, index) => {
         const city: any = (await query("SELECT id,name FROM cities WHERE name = ?;", [stop.city])).map((city) => ({ stop_name: city.name, id: city.id, network: 0, city: city.name }))[0]
         if (!cities.includes(stop.city)) {
